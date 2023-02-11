685aeec8565804855dda3a22899604490e23d009
Jay Flatland <jayflatland@gmail.com>
2023-02-11 15:41:09 -0600

    Fixed and working

--- wordle/cpp/find_best_first_three_words.hpp
+++ wordle/cpp/find_best_first_three_words.hpp
@@ -12,40 +12,6 @@
 #include "match_result.hpp"
 #include "util.hpp"
 
-inline match_results_type fast_calc_match_results(const word_type &solution, const word_type &guess)
-{
-    auto results = match_results_type();
-    word_type solution_copy = solution;
-    word_type guess_copy = guess;
-
-    // copy in word
-    results.set_guess(guess);
-
-    // check for greens first
-    for (size_t i = 0; i < 5; i++)
-    {
-        if (guess_copy[i] == solution_copy[i])
-        {
-            results.pos_[i].color_ = color_type::green;
-            solution_copy[i] = '_';
-            guess_copy[i] = '*';
-        }
-    }
-
-    // check for yellows
-    for (size_t i = 0; i < 5; i++)
-    {
-        if (solution_copy.contains(guess_copy[i]))
-        {
-            results.pos_[i].color_ = color_type::yellow;
-            solution_copy.remove(guess_copy[i]);
-            // FASTER (not really) guess_copy[i] = '*';
-        }
-    }
-
-    return results;
-}
-
 using test_type = std::vector<word_type>;
 
 // struct test_type
@@ -56,7 +22,7 @@ using test_type = std::vector<word_type>;
 template <typename StreamT>
 StreamT &operator<<(StreamT &os, const test_type &x)
 {
-    os << "{\"";
+    os << "{";
     bool first = true;
     for (auto &&w : x)
     {
@@ -65,9 +31,10 @@ StreamT &operator<<(StreamT &os, const test_type &x)
             os << ",";
         }
         first = false;
-        os << '"' << w << '"';
+        // os << '"' << w << '"';
+        os << w;
     }
-    os << "\"}";
+    os << "}";
     return os;
 }
 
@@ -86,29 +53,96 @@ inline int color_to_int(const color_type &color)
     return 0;
 }
 
-inline int match_result_to_int(const match_results_type &m)
+struct packed_test_result_type// : uint64_t
 {
-    int r = 0;
+public:
+    packed_test_result_type() : x_(0) {}
+    packed_test_result_type(const uint64_t &x)
+        : x_(x)
+    {
+    }
+    
+    static constexpr uint64_t digit_mult() { return 3; }
+    static constexpr uint64_t word_mult() { return 243; } // 3**5
+
+    packed_test_result_type operator*(const packed_test_result_type& rhs) const
+    {
+        return packed_test_result_type(x_ * rhs.x_);
+    }
+
+    packed_test_result_type operator+(const packed_test_result_type& rhs) const
+    {
+        return packed_test_result_type(x_ + rhs.x_);
+    }
+    
+    bool operator==(const packed_test_result_type& rhs) const
+    {
+        return x_ == rhs.x_;
+    }
+
+    // //easier debug
+    // static const int digit_mult = 10;
+    // static const int word_mult = 100000;
+    
+    template <typename StreamT>
+    friend StreamT& operator<<(StreamT& os, const packed_test_result_type& rhs)
+    {
+        std::string s;
+        uint64_t x = rhs.x_;
+        if(x == 0) { s = "0"; }
+        while(x > 0)
+        {
+            //os << (x%digit_mult());
+            s = std::to_string(x%digit_mult()) + s;
+            x /= digit_mult();
+        }
+        os << s;
+        return os;
+    }
+    
+    const auto& val() const { return x_; }
+private:
+    uint64_t x_;
+};
+
+
+template<>
+struct std::hash<packed_test_result_type>
+{
+    std::size_t operator()(const packed_test_result_type& x) const
+    {
+        return std::hash<uint64_t>{}(x.val());
+    }
+};
+
+// using packed_test_result_type = uint64_t;//not sure i can pack 3 words into u32
+
+// using packed_test_result_type = uint64_t;
+// static const packed_test_result_type packed_test_result_digit_mult = 10;
+// static const packed_test_result_type packed_test_result_word_mult = 100000;
+
+inline packed_test_result_type match_result_to_int(const match_results_type &m)
+{
+    packed_test_result_type r;
     for (auto &&lc : m.pos_)
     {
         auto &&c = lc.color_;
-        r = r * 3 + color_to_int(c);
+        r = r * packed_test_result_type::digit_mult() + color_to_int(c);
     }
     return r;
 }
 
-inline int word_solution_to_int(const word_type &word, const word_type &solution)
+inline packed_test_result_type word_solution_to_int(const word_type &solution, const word_type &word)
 {
-    return match_result_to_int(fast_calc_match_results(solution, word));
+    return match_result_to_int(calc_match_results(solution, word));
 }
 
-inline int calc_test_result(const test_type &tst, const word_type &solution)
+inline packed_test_result_type calc_test_result(const test_type &tst, const word_type &solution)
 {
-    static const int max_word_result = 243; // 3**5
-    int r = 0;
+    packed_test_result_type r;
     for (auto &&w : tst)
     {
-        r = r * max_word_result + word_solution_to_int(solution, w);
+        r = r * packed_test_result_type::word_mult() + word_solution_to_int(solution, w);
     }
     return r;
 }
@@ -118,14 +152,15 @@ class score_type
 public:
     double score() const
     {
-        // return -max_;
+        return -100.0 * max_ - avg();
         // return cnt_;
-        return -avg();
+        // return -avg();
     }
 
     int max_ = 0;
     int total_ = 0;
     int cnt_ = 0;
+    packed_test_result_type mr_;
 
     double avg() const
     {
@@ -133,21 +168,22 @@ public:
     }
 
     bool operator<(const score_type &rhs) const { return score() < rhs.score(); }
+    bool operator<=(const score_type &rhs) const { return score() <= rhs.score(); }
 };
 
 template <typename StreamT>
 StreamT &operator<<(StreamT &os, const score_type &x)
 {
-    os << "(cnt=" << x.cnt_ << ", max=" << x.max_ << ", avg=" << x.avg() << ", score=" << x.score() << ")";
+    os << "(cnt=" << x.cnt_ << ", max=" << x.max_ << ", avg=" << x.avg() << ", score=" << x.score() << ", mr=" << x.mr_ << ")";
     return os;
 }
 
 inline score_type calc_score_for_test(const word_list_type &words, const test_type &test)
 {
-    std::unordered_map<int, int> counts;
+    std::unordered_map<packed_test_result_type, int> counts;
     for (auto &&s : words)
     {
-        int r = calc_test_result(test, s);
+        packed_test_result_type r = calc_test_result(test, s);
         counts[r]++;
     }
 
@@ -158,11 +194,12 @@ inline score_type calc_score_for_test(const word_list_type &words, const test_ty
         s.total_ += cnt;
         s.max_ = std::max(s.max_, cnt);
         s.cnt_++;
+        s.mr_ = kv.first;
     }
     return s;
 }
 
-test_type random_mutate_test(const word_list_type &words, const test_type &parent)
+inline test_type random_mutate_test(const word_list_type &words, const test_type &parent)
 {
     int wn = random() % parent.size();
     int wi = random() % words.size();
@@ -173,7 +210,7 @@ test_type random_mutate_test(const word_list_type &words, const test_type &paren
     return child;
 }
 
-test_type cycle_mutate_test(const word_list_type &words, const test_type &parent)
+inline test_type cycle_mutate_test(const word_list_type &words, const test_type &parent)
 {
     static int wi = 0;
     static int wn = 0;
@@ -205,6 +242,7 @@ inline void find_best_first_three_words()
     =========================================================================*/
 
     auto words = read_5_letter_words("wordle_words.txt");
+    // auto words = read_5_letter_words("wordle_solutions.txt");
     auto solutions = read_5_letter_words("wordle_solutions.txt");
     // auto words = read_5_letter_words("wordle_solutions.txt");
     // auto words = read_5_letter_words("corncob_lowercase.txt");
@@ -216,17 +254,13 @@ inline void find_best_first_three_words()
     std::cout << words.size() << " words" << std::endl;
     std::cout << solutions.size() << " solutions" << std::endl;
 
-    // test_type best_test{words.front(), words.front(), words.front()};
-    test_type best_test{"aesir"};
-    // test_type best_test{"corse","palet","bundt"};//(cnt=1836, max=9, avg=1.26089, score=1836);//wordle words/solutions, cnt
-    // test_type best_test{"corse", "palet", "wings"}; //=>(cnt=1749, max=6, avg=1.32361, score=-6)//wordle words/solutions, max
-    // test_type best_test{"aesir","cloth","bungy"};
-    // test_type best_test{"meros","patly","deink"};
-    // test_type best_test{"colts","spare","admin"};
-    // best={"skran","cloth","derig"}=>-26
-    // best={"freak","cloth","admin"}=>-7 wordl solutions, max
-    // best={"cloth","kesar","admin"}=>-28, dwyl, max
-    // best={"meros","patly","deink"}=>-26, dwyl, max
+    //test_type best_test{words.front(), words.front(), words.front()};
+    test_type best_test{"homed", "clang", "sprit"};
+    // test_type best_test{words.front(), words.front(), words.front(), words.front()};
+    // test_type best_test{words.front()};
+    // test_type best_test{words.front(), words.front()};
+    // blast,viper,downy - solutions only
+    // homed,clang,sprit - wordle words and solutions
     score_type best_score = calc_score_for_test(solutions, best_test);
     std::cout << "best=" << best_test << "=>" << best_score << std::endl;
 
@@ -274,9 +308,12 @@ inline void find_best_first_three_words()
     //block,blond,blood,bloom,blown,bluff,blunt,bobby,bongo,booby,booth,booty,boozy,botch,bough,bound,buddy,buggy,bulky,bully,bunch,bunny,butch,buxom,chock,chuck,chump,chunk,clock,cloth,cloud,clout,clown,cluck,clump,clung,colon,comfy,conch,condo,couch,cough,could,count,coyly,dodgy,dolly,donut,doubt,dough,dowdy,downy,duchy,dully,dummy,dumpy,dutch,flock,flood,flout,flown,fluff,flung,flunk,foggy,folly,found,fully,funky,funny,fuzzy,ghoul,gloom,glyph,godly,golly,goody,goofy,gulch,gully,gumbo,gummy,guppy,hobby,holly,hotly,hound,howdy,humph,hunch,hunky,hutch,jolly,jumbo,jumpy,junto,knock,knoll,known,lobby,lofty,loopy,lowly,lucky,lumpy,lunch,lymph,lynch,mogul,moldy,month,moody,motto,moult,mound,mount,mouth,mucky,muddy,mulch,mummy,munch,nobly,notch,nutty,nylon,nymph,oddly,ought,outdo,outgo,phony,photo,pluck,plumb,plump,plunk,polyp,pooch,poppy,pouch,pound,pouty,pudgy,puffy,pulpy,punch,puppy,putty,pygmy,quoth,thong,thumb,thump,toddy,tooth,touch,tough,uncut,vouch,whoop,woody,wooly,woozy,would,wound,young,youth
 
     new way is counting wrong somehow, gets 199 instead of 168
-    
+
     AHA!!!  new way is incorrect - for a guess word, not all solutions with the same
     color pattern are the same
+
+    AHA AGAIN - the match result was inverted.  old way is agnostic to A vs B or B vs A
+    in the match result calc.  new way is sensitive.
     */
 
     // brute force search
