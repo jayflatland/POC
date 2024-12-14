373e09e39b18616eedef56c80af84a2823c05e68
Jay Flatland <jay@teamclearsky.com>
2024-12-14 17:08:55 -0600

    Ignores

--- sdr/test2.py
+++ sdr/test2.py
@@ -3,19 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sdr = RtlSdr()
-sdr.sample_rate = 3e6 # Hz
-sdr.center_freq = 101.1e6   # Hz
+sdr.sample_rate = 3.2e6 # Hz
+sdr.center_freq = 910e6   # Hz
 #sdr.freq_correction = 60  # PPM
 #print(sdr.valid_gains_db)
-sdr.gain = 49.6
+# sdr.gain = 49.6
 #print(sdr.gain)
 
-x = sdr.read_samples(2<<20)
-# x = x[-4096:]
+x = sdr.read_samples(4<<10)
+x = x[2048:]
 sdr.close()
 
-# plt.figure()
-# plt.plot(x.real, label="I")
-# plt.plot(x.imag, label="Q")
-# plt.legend()
-# plt.show()
+# %%
+plt.figure()
+plt.plot(x.real, label="I")
+plt.plot(x.imag, label="Q")
+plt.legend()
+plt.show()
--- sdr/uhd_testing/uhd_test01.cpp
+++ sdr/uhd_testing/uhd_test01.cpp
@@ -1,15 +1,126 @@
+#include <uhd/convert.hpp>
+#include <uhd/exception.hpp>
+#include <uhd/types/tune_request.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+#include <boost/program_options.hpp>
+#ifdef __linux__
+#    include <boost/filesystem.hpp>
+#    include <boost/process.hpp>
+#endif
+#include <chrono>
+#include <complex>
+#include <csignal>
+#include <fstream>
 #include <iostream>
-#include <uhd.h>
-#include <uhd/device.hpp>
+#include <numeric>
+#include <regex>
+#include <thread>
 
-int main()
+using namespace std::chrono_literals;
+
+namespace po = boost::program_options;
+
+//args:
+//name,product=B200,serial=F5C23F,type=b200,uhd
+
+int UHD_SAFE_MAIN(int argc, char* argv[])
 {
-    uhd::device_addr_t hint;
-    hint["type"] = "usrp1";
-    uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
+    // variables to be set by po
+    std::string args, file, type, ant, subdev, ref, wirefmt, channels;
+    size_t total_num_samps, spb;
+    double rate, freq, gain, bw, total_time, setup_time, lo_offset;
+
+    std::vector<std::thread> threads;
+
+    std::vector<size_t> channel_list;
+    std::vector<std::string> channel_strings;
+    // setup the program options
+    po::options_description desc("Allowed options");
+    // clang-format off
+    desc.add_options()
+        ("help", "help message")
+        ("args", po::value<std::string>(&args)->default_value(""), "multi uhd device address args")
+        ("file", po::value<std::string>(&file)->default_value("usrp_samples.dat"), "name of the file to write binary samples to")
+        ("type", po::value<std::string>(&type)->default_value("short"), "sample type: double, float, or short")
+        ("nsamps", po::value<size_t>(&total_num_samps)->default_value(0), "total number of samples to receive")
+        ("duration", po::value<double>(&total_time)->default_value(0), "total number of seconds to receive")
+        ("spb", po::value<size_t>(&spb)->default_value(10000), "samples per buffer")
+        ("rate", po::value<double>(&rate)->default_value(1e6), "rate of incoming samples")
+        ("freq", po::value<double>(&freq)->default_value(0.0), "RF center frequency in Hz")
+        ("lo-offset", po::value<double>(&lo_offset)->default_value(0.0),
+            "Offset for frontend LO in Hz (optional)")
+        ("gain", po::value<double>(&gain), "gain for the RF chain")
+        ("ant", po::value<std::string>(&ant), "antenna selection")
+        ("subdev", po::value<std::string>(&subdev), "subdevice specification")
+        ("channels,channel", po::value<std::string>(&channels)->default_value("0"), "which channel(s) to use (specify \"0\", \"1\", \"0,1\", etc)")
+        ("bw", po::value<double>(&bw), "analog frontend filter bandwidth in Hz")
+        ("ref", po::value<std::string>(&ref), "reference source (internal, external, mimo)")
+        ("wirefmt", po::value<std::string>(&wirefmt)->default_value("sc16"), "wire format (sc8, sc16 or s16)")
+        ("setup", po::value<double>(&setup_time)->default_value(1.0), "seconds of setup time")
+        ("progress", "periodically display short-term bandwidth")
+        ("stats", "show average bandwidth on exit")
+        ("sizemap", "track packet size and display breakdown on exit. Use with multi_streamer option if CPU limits stream rate.")
+        ("null", "run without writing to file")
+        ("continue", "don't abort on a bad packet")
+        ("skip-lo", "skip checking LO lock status")
+        ("int-n", "tune USRP with integer-N tuning")
+        ("multi_streamer", "Create a separate streamer per channel.")
+    ;
+    // clang-format on
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // print the help message
+    if (vm.count("help")) {
+        std::cout << "UHD RX samples to file " << desc << std::endl;
+        std::cout << std::endl
+                  << "This application streams data from a single channel of a USRP "
+                     "device to a file.\n"
+                  << std::endl;
+        return ~0;
+    }
+
+    bool bw_summary             = vm.count("progress") > 0;
+    bool stats                  = vm.count("stats") > 0;
+    bool null                   = vm.count("null") > 0;
+    bool enable_size_map        = vm.count("sizemap") > 0;
+    bool continue_on_bad_packet = vm.count("continue") > 0;
+    bool multithread            = vm.count("multi_streamer") > 0;
+
+    if (enable_size_map)
+        std::cout << "Packet size tracking enabled - will only recv one packet at a time!"
+                  << std::endl;
+
+    // create a usrp device
+    std::cout << std::endl;
+    std::cout << "Creating the usrp device with: " << args << "..." << std::endl;
+    uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(args);
+
+
+
+
+    // uhd::device_addr_t hint;
+    // // hint["type"] = "usrp1";
+    // uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
+    // for(auto &&dev : dev_addrs)
+    // {
+    //     std::cout << "device:" << std::endl;
+    //     auto &&keys = dev.keys();
+    //     auto &&vals = dev.vals();
+    //     for(int i = 0; i < keys.size(); i++)
+    //     {
+    //         std::cout << keys[i] << "=" << vals[i] << std::endl;
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     // uhd_string_vector_handle devs;
     // uhd_usrp_find(nullptr, &devs);
     // std::cout << "Hello world!" << std::endl;
     return 0;
 }
+
